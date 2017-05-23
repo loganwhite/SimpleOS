8ef6291e1c2f63c2947fd6b878963793145e39c6
loganwhite <loganwhite@163.com>
2017-05-23 21:40:29 +0800

    add linker script

---
+++ src/core/flags.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: flags.h
+	> Author: Logan Von
+	> Mail: loganwhitevon@gmail.com
+	> Created Time: Tue 23 May 2017 08:44:50 PM CST
+ ************************************************************************/
+
+#ifndef _FLAGS_H
+#define _FLAGS_H
+
+/* EFLAGES Register */
+#define FLAG_MBS 0x00000000      /* Must be set. */
+#define FLAG_IF  0x00000200      /* Interrupt Flag. */
+
+#endif  /* End of flag.h */
